2d0f70f4b030573a4d78ac275b845cfea2b13570
Jean Marcellin <marcellinja@gmail.com>
2018-02-15 09:28:55 -0800

    - listed all the bookmarks in continuous list

--- bookIt.js
+++ bookIt.js
@@ -23,35 +23,73 @@
   //   document.body.appendChild(beastImage);
   // }
 
-  function makeIndent(indentLength) {
-    return ".".repeat(indentLength);
-  }
+  // function openListItem(id,url) {
+  //   return "<li id="+id+">" + url;
+  // }
 
-  function logItems(bookmarkItem, indent) {
-    if (bookmarkItem.url) {
-      console.log(makeIndent(indent) + bookmarkItem.url);
-    } else {
-      console.log(makeIndent(indent) + "Folder");
-      indent++;
-    }
-    if (bookmarkItem.children) {
-      for (child of bookmarkItem.children) {
-        logItems(child, indent);
+  // function closeListItem(id,url) {
+  //   return "</li>";
+  // }
+
+  // function openList(id,url) {
+  //   return "<ul id="+id+">";
+  // }
+
+  // function closeList() {
+  //   return "</ul>";
+  // }
+
+  // function ListItems(bookmarkItem, level, parent) {
+  //   // openList(bookmarkItem.id)
+  //   let li = document.createElement('li').setAttribute('id',bookmarkItem.id);
+  //   if (bookmarkItem.url) {
+  //     openListItem(bookmarkItem.id, bookmarkItem.url);
+  //   } else {
+  //     makeList(makeIndent(indent) + "Folder");
+  //     level++;
+  //   }
+  //   if (bookmarkItem.children) {
+  //     for (child of bookmarkItem.children) {
+  //       logItems(child, indent, bookmarkItem);
+  //     }
+  //   }
+
+  function buildList(data, isSub){
+    var html = (isSub)?'<div>':''; // Wrap with div if true
+    html += '<ul>';
+    for(item in data){
+      html += '<li>';
+        if(typeof(data[item].children) === 'object'){ // An array will return 'object'
+          if(isSub){
+            html += '<a href="' + data[item].url + '">' + data[item].name + '</a>';
+          } else {
+                html += data[item].id; // Submenu found, but top level list item.
+              }
+            html += buildList(data[item].children, true); // Submenu found. Calling recursively same method (and wrapping it in a div)
+          } else {
+            html += data[item].id // No submenu
+          }
+          html += '</li>';
+        }
+        html += '</ul>';
+        html += (isSub)?'</div>':'';
+        return html;
       }
+
+      //   closeList();
+      //   level--;
+      // }
+
+    function buildTree(bookmarkItems) {
+      document.getElementById("container").innerHTML = buildList(bookmarkItems, false);
     }
-    indent--;
-  }
 
-  function logTree(bookmarkItems) {
-    logItems(bookmarkItems[0], 0);
-  }
+    function onRejected(error) {
+      console.log(`An error: ${error}`);
+    }
 
-  function onRejected(error) {
-    console.log(`An error: ${error}`);
-  }
-
-  var gettingTree = browser.bookmarks.getTree();
-  gettingTree.then(logTree, onRejected);
+    var gettingTree = browser.bookmarks.getTree();
+    gettingTree.then(buildTree, onRejected);
 
   /**
    * Listen for messages from the background script.
