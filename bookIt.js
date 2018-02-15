4a94878688fb98a10694dff48dfc74d872f60e32
Jean Marcellin <marcellinja@gmail.com>
2018-02-15 09:30:36 -0800

    - code cleanup

--- bookIt.js
+++ bookIt.js
@@ -23,62 +23,27 @@
   //   document.body.appendChild(beastImage);
   // }
 
-  // function openListItem(id,url) {
-  //   return "<li id="+id+">" + url;
-  // }
-
-  // function closeListItem(id,url) {
-  //   return "</li>";
-  // }
-
-  // function openList(id,url) {
-  //   return "<ul id="+id+">";
-  // }
-
-  // function closeList() {
-  //   return "</ul>";
-  // }
-
-  // function ListItems(bookmarkItem, level, parent) {
-  //   // openList(bookmarkItem.id)
-  //   let li = document.createElement('li').setAttribute('id',bookmarkItem.id);
-  //   if (bookmarkItem.url) {
-  //     openListItem(bookmarkItem.id, bookmarkItem.url);
-  //   } else {
-  //     makeList(makeIndent(indent) + "Folder");
-  //     level++;
-  //   }
-  //   if (bookmarkItem.children) {
-  //     for (child of bookmarkItem.children) {
-  //       logItems(child, indent, bookmarkItem);
-  //     }
-  //   }
-
-  function buildList(data, isSub){
-    var html = (isSub)?'<div>':''; // Wrap with div if true
+  function buildList(data, isChild){
+    var html = (isChild)?'<div>':''; // Wrap with div if true
     html += '<ul>';
     for(item in data){
       html += '<li>';
         if(typeof(data[item].children) === 'object'){ // An array will return 'object'
-          if(isSub){
+          if(isChild){
             html += '<a href="' + data[item].url + '">' + data[item].name + '</a>';
           } else {
-                html += data[item].id; // Submenu found, but top level list item.
+                html += data[item].id; // Child found, but top level list item.
               }
-            html += buildList(data[item].children, true); // Submenu found. Calling recursively same method (and wrapping it in a div)
+            html += buildList(data[item].children, true); // Child found. Calling recursively same method (and wrapping it in a div)
           } else {
-            html += data[item].id // No submenu
+            html += data[item].id // No Child
           }
           html += '</li>';
         }
         html += '</ul>';
-        html += (isSub)?'</div>':'';
+        html += (isChild)?'</div>':'';
         return html;
       }
-
-      //   closeList();
-      //   level--;
-      // }
 
     function buildTree(bookmarkItems) {
       document.getElementById("container").innerHTML = buildList(bookmarkItems, false);
